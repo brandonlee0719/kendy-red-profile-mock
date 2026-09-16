6538c22278775318671a639d46a01074828057d9
coolxerxes14 <coolxerxes14@gmail.com>
2026-09-15 21:41:46 -0400

    fix error

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,7 +14,6 @@ import {
   Large,
   Chat,
   Empty,
-  FooterContainer,
   Language,
   Label,
   LanguageSwitch,
@@ -29,20 +28,6 @@ interface SocialLinkProps {
 const Footer = ({ t }: { t: TFunction }) => {
   const handleChange = (language: string) => {
     i18n.changeLanguage(language);
-  };
-
-  const SocialLink = ({ href, src }: SocialLinkProps) => {
-    return (
-      <a
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-        key={src}
-        aria-label={src}
-      >
-        <SvgIcon src={src} width="25px" height="25px" />
-      </a>
-    );
   };
 
   return (
